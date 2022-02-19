7b50578d916590bb4d851309de627110f91fa3f6
sweetdaniela23 <90408575+sweetdaniela23@users.noreply.github.com>
2022-02-18 18:46:07 -0700

    Add project files.

---
+++ Infrastructure/PaginationTagHelper.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WaterProject.Models.ViewModels;
+
+namespace WaterProject.Infrastructure
+{
+    [HtmlTargetElement("div", Attributes = "page-blah")]
+    public class PaginationTagHelper :TagHelper
+    {
+        //dynamically create the page links for us
+        private IUrlHelperFactory uhf;
+        public PaginationTagHelper (IUrlHelperFactory temp)
+        {
+            uhf = temp;
+        }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext vc { get; set; }
+
+        //different than view context
+        public PageInfo PageBlah { get; set; }
+        public string PageAction { get; set; }
+
+        //override method
+        public override void Process(TagHelperContext thc, TagHelperOutput tho)
+        {
+            IUrlHelper uh = uhf.GetUrlHelper(vc);
+
+            TagBuilder final = new TagBuilder("div");
+
+            for (int i = 1; i < PageBlah.TotalPages; i++)
+            {
+                TagBuilder tb = new TagBuilder("a");
+                tb.Attributes["href"] = uh.Action(PageAction, new { pageNum = i });
+                tb.InnerHtml.Append(i.ToString());
+
+                final.InnerHtml.AppendHtml(tb);
+            }
+
+            tho.Content.AppendHtml(final.InnerHtml);
+        }
+    }
+}
---
+++ Models/ViewModels/PageInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WaterProject.Models.ViewModels
+{
+    public class PageInfo
+    {
+        public int TotalNumProjects { get; set; }
+        public int ProjectsPerPage { get; set; }
+        public int CurrentPage { get; set; }
+        //figure out how many pages I need
+        public int TotalPages => (int) Math.Ceiling((double) TotalNumProjects / ProjectsPerPage);
+    }
+}
